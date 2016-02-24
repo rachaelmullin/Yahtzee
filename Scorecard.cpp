5041daa3eba02283df82a4aa9e8786d4681619db
NoahSarkey <nsarkey@nd.edu>
2016-02-24 13:13:24 -0500

    Create Scorecard.cpp
    Contains function definitions for Scorecard.h

---
+++ Scorecard.cpp
@@ -0,0 +1,33 @@
+// Pauline Blatt, Rachael Mullin, Noah Sarkey
+// Final Project
+// Scorecard.cpp
+
+
+Scorecard::Scorecard()
+{
+
+
+
+}	//end constructor
+
+void Scorecard::calculateScore()
+{
+
+
+
+}	//end of calculateScore function
+
+void Scorecard::print()
+{
+
+
+
+
+}	//end of print function
+
+void Scorecard::giveBonus()
+{
+
+
+
+}	//end of giveBonus function
