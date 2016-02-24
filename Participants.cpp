fd63cdd434824f689a208a709a12a544666802a0
NoahSarkey <nsarkey@nd.edu>
2016-02-24 13:15:26 -0500

    Create Participants.cpp
    Contains function definitions from Participants.h

---
+++ Participants.cpp
@@ -0,0 +1,17 @@
+// Pauline Blatt, Rachael Mullin, Noah Sarkey
+// Final Project
+// Participants.cpp
+
+Participants::Participants()
+{
+
+
+
+}	//end of default constructor
+
+void Participants::roll()
+{
+
+
+
+}	//end of roll function
