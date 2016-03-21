6177d9e0a33fc7d2e93a29aef296df807233fe96
Pauline Mae Blatt <pblatt@student00.cse.nd.edu>
2016-03-20 23:16:49 -0400

    Roll.cpp edited

--- roll.cpp
+++ roll.cpp
@@ -1,1 +1,25 @@
-sdfasdfsa
+// roll.cpp
+// testing the roll function to put into the Yahtzee program
+
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+using namespace std;
+
+int main() {
+
+    int roll=1;
+    int die[5];
+
+    while (roll<2) {
+        srand(time(NULL));
+        
+        for (int i=0; i<6; i++) {
+            die[i] = rand() % 6 + 1;
+            cout << "Die " << i+1 << "= " << die[i] << endl;
+        }
+        roll++;
+        
+    }
+}
+
