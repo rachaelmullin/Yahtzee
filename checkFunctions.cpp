c7aca6cdc0f7fc32886376f163112dd153de6771
Rachael M Mullin <rmullin@student00.cse.nd.edu>
2016-03-20 23:24:23 -0400

    finished up the functions that validate and compute the score for a specific square

--- checkFunctions.cpp
+++ checkFunctions.cpp
@@ -6,72 +6,94 @@
 #include <iostream>
 using namespace std;
 
-int checkAce(player) {
+
+int checkAce(int,int);
+int checkTwos(int, int);
+int checkThrees(int, int);
+int checkFours(int, int);
+int checkFives(int, int);
+int checkSix(int, int);
+int check3ofKind(int, int);
+int check4ofKind(int, int);
+int checkFullHouse(int, int);
+int checkSmStraight(int, int);
+int checkLgStraight(int, int);
+int checkYahtzee(int, int);
+int checkChance(int, int);
+
+int main() {
+
+}
+
+int checkAce(int player, int die[5]) {
 //take in die array, check if  valid, compute score for that square on scorecard
-	int count=0;
+	int count=0, total;
 	for (int i=0; i<5; i++) {
 		if (die[i]==1) {
 			count++;
 		}
 	}
-	scorecard[player][0]=count*1;
+	total=count*1;
+	return total;
 }
 
-int checkTwos() {
-	int count=0;
+int checkTwos(int player, int die[5]) {
+	int count=0, total;
 	for (int i=0; i<5; i++) {
                 if (die[i]==2) {
                         count++;
                 }
         }
-        scorecard[player][1]=count*1;
+        total=count*1;
+	return total;
 }
 
-int checkThrees() {
-	int count=0;
+int checkThrees(int player, int die[5]) {
+	int count=0, total;
         for (int i=0; i<5; i++) {
                 if (die[i]==3) {
                         count++;
                 }
         }
-        scorecard[player][2]=count*1;
-
+        total=count*1;
+	return total;
 }
 
-int checkFours() {
-	int count=0;
+int checkFours(int player, int die[5]) {
+	int count=0, total;
         for (int i=0; i<5; i++) {
                 if (die[i]==4) {
                         count++;
                 }
         }
-        scorecard[player][3]=count*1;
-
+        total=count*1;
+	return total;
 }
 
-int checkFives() {
-	int count=0;
+int checkFives(int player, int die[5]) {
+	int count=0, total; 
         for (int i=0; i<5; i++) {
                 if (die[i]==5) {
                         count++;
                 }
         }
-        scorecard[player][4]=count*1;
+        total=count*1;
+	return total;
 }
 
-int checkSix() {
-	int count=0;
+int checkSix(int player, int die[5]) {
+	int count=0, total;
         for (int i=0; i<5; i++) {
                 if (die[i]==6) {
                         count++;
                 }
         }
-        scorecard[player][5]=count*1;
-
+        total=count*1;
+	return total;
 }
 
-int check3ofKind() {
-	int valid=0;
+int check3ofKind(int player, int die[2]) {
+	int valid=0, total;
 	for (int i=0; i<5; i++) {
 		int count=0;
 		for (int j=0; j<5; j++) {
@@ -84,13 +106,14 @@ int check3ofKind() {
 		if (count==2) break;
 	}
 	if (valid != 0) {
-		scorecard[player][8]=valid*3;
+		total=valid*3;
 	}
-	else scorecard[player][8]=0;
+	else total=0;
+	return total;
 }
 
-int check4ofKind() {
-	int valid=0;
+int check4ofKind(int player, int die[5]) {
+	int valid=0, total;
         for (int i=0; i<5; i++) {
                 int count=0;
                 for (int j=0; j<5; j++) {
@@ -103,13 +126,14 @@ int check4ofKind() {
                 if (count==3) break;
         }
         if (valid != 0) {
-                scorecard[player][9]=valid*4;
+                total=valid*4;
         }
-        else scorecard[player][9]=0;
+        else total=0;
+	return total;
 }
 
-int checkFullHouse() {
-	int pair=0, pairNum, triple=0, tripleNum;
+int checkFullHouse(int player, int die[5]) {
+	int total, pair=0, pairNum, triple=0, tripleNum;
 	for (int i=0; i<5; i++) {
 		for (int j=0; j<5; j++) {
 			if (i!=j && die[i]==die[j]) {
@@ -131,21 +155,66 @@ int checkFullHouse() {
 		}
         }
 	if (pair=1 && triple==1) {
-		scorecard[player][10]=25;
+		total=25;
 	}
-	else scorecard[player][10]=0;
+	else total=0;
+	return total;
 }
 
-int checkSmStraight() {
-	
+int checkSmStraight(int player, int die[5]) {
+	int total, array[5], temp, valid;
+	for (int i=0; i<5; i++) {
+		array[i]=die[i];
+	}
+	for (int j=0; j<4; j++) {
+		for (int k=0; k<4-4; k++) {
+			if (array[k]>array[k+1]) {
+				temp=array[k];
+				array[k]=array[k+1];
+				array[k+1]=temp;
+			}
+		}
+	}
+	for (int i=0; i<4; i++) {
+		if (array[i]==array[i+1]+1) valid=1;
+		else valid=0;
+	}
+	if (valid==0) {
+		for (int i=1; i<5; i++) {
+			if (array[i]==array[i+1]+1) valid=1;
+			else valid=0;
+		}
+	}
+	if (valid==1) total=30;
+	else total=0;
+	return total;		
 }
 
-int checkLgStraight() {
-
+int checkLgStraight(int player, int die[5]) {
+	int total, array[5], temp, valid;
+        for (int i=0; i<5; i++) {
+                array[i]=die[i];
+        }
+        for (int j=0; j<4; j++) {
+                for (int k=0; k<4-4; k++) {
+                        if (array[k]>array[k+1]) {
+                                temp=array[k];
+                                array[k]=array[k+1];
+                                array[k+1]=temp;
+                        }
+                }
+        }       
+	for (int i=0; i<5; i++) {
+		if (array[i]==array[i+1]+1) valid=1;
+		else valid=0;
+	}
+	if (valid==1) total=40;
+	else total=0;	
+	return total;
 }
 
-int checkYahtzee() {
-	int valid=0;
+int checkYahtzee(int player, int die[5]) {
+	int total, valid=0;
 	for (int i=0; i<5; i++) {
 		int count=0;
 		for (int j=0; j<5; j++) {
@@ -157,11 +226,13 @@ int checkYahtzee() {
 			valid=1;
 		}
 	}
-	if (valid==1) scorecard[player][13]=50;
-	else scorecard[player][13]=0;
+	if (valid==1) total=50;
+	else total=0;
+	return total;
 }
 
-int checkChance() {
-	scorecard[player][14]=die[0]+die[1]+die[2]+die[3]+die[4];
+int checkChance(int player, int die[5]) {
+	int total=die[0]+die[1]+die[2]+die[3]+die[4];
+	return total;
 }
 
